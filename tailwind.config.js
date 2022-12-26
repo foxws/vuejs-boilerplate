af7cbf9fed87de7f7f5fa354ec91aa67bc882d5f
francoism90 <francoism90@users.noreply.github.com>
2022-12-26 09:52:10 +0100

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "rgb(var(--color-primary) / <alpha-value>)",
+        secondary: "rgb(var(--color-secondary) / <alpha-value>)",
+      },
+    },
   },
   plugins: [],
 };
