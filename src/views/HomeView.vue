b01cdcf18ce26ff68d0739f75ca895c1cc8d1e81
francoism90 <francoism90@users.noreply.github.com>
2022-12-29 16:36:25 +0100

    wip

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container mx-auto py-10 px-5">
     <section>
-      <h2 class="font-subtitle text-white">Picks for you</h2>
-      <h1 class="font-title text-white">Picks for you</h1>
+      <h2 class="font-subtitle">Picks for you</h2>
+      <h1 class="font-title">Picks for you</h1>
     </section>
   </div>
 </template>
